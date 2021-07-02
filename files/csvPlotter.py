ec39dd49c234e1db22634110eed10b1d139ec3e1
Shajiryan <84907245+Shajiryan@users.noreply.github.com>
2021-07-02 17:25:21 +0200

    Update csvPlotter.py

--- files/csvPlotter.py
+++ files/csvPlotter.py
@@ -1,41 +1,50 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
-Time_Counter = 1
-Time = []
+
+Counter = 0
 Temperature = []
 Humidity = []
 Height = []
+VertSpeed = []
+WindSpeed = []
 
 with open('Radiosonde_O2100002_2021-01-27-12.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
 
-    for line in csv_reader:
-        
-        Time.append(Time_Counter)
-        Temperature.append(line[10])
-        Humidity.append(line[12])
-        Height.append(line[4])
-        Time_Counter +=1
-        
-        if Time_Counter == 1000:    #break nach den ersten 1000 sec (erstmal)
-            break
-        
-        
-    fig, axs = plt.subplots(3)
-    axs[0].plot(Time, Temperature)
-    axs[0].set_ylabel('Temperature')
-    axs[0].set_yticks([0, 15, 25, 35, 45, 52])
-    
-    axs[1].plot(Time, Humidity)
-    axs[1].set_ylabel('Humidity')
-    axs[1].set_yticks([0, 50, 100, 150, 190])
-    
-    axs[2].plot(Time, Height)
-    axs[2].set_ylabel('Height')
-    axs[2].set_yticks([0, 100,200,300,400,500])
+    for line in csv_reader:            
+
+        if not line[12] or not line[10] or not line[4] or not line[9] or not line[7]:
+            pass
+        else:
+            WindSpeed.append(float(line[7]))
+            VertSpeed.append(float(line[9]))
+            Temperature.append(float(line[10]))
+            Humidity.append(float(line[12]))
+            Height.append(float(line[4]))
+            Counter +=1   
 
 
+    fig, (ax0, ax1, ax2, ax3) = plt.subplots(1,4, sharey=True)
+
+    ax0.plot(Temperature, Height)
+    ax0.set_ylabel('Height[m]')
+    ax0.set_xlabel('Temperature[°C]')
+    ax0.grid()
+
+    ax1.plot(Humidity, Height)
+    ax1.set_xlabel('Humidity[%]')
+    ax1.grid()
+
+    ax2.plot(VertSpeed, Height)
+    ax2.set_xlabel('Vertical Speed[m/s]')
+    ax2.grid()
+
+    ax3.plot(WindSpeed, Height)
+    ax3.set_xlabel('Wind Speed[m/s]')
+    ax3.grid()
+    
     plt.show()
